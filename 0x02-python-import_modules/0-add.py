6284e2503314e50188d28cd02e11fbec6454e530
Jbargotio <jbargotio@gmail.com>
2022-06-02 03:06:59 -0700

    Importing function add from add_0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+# 0_add.py
 
-if __name__ == "__main":
-    from add_0 import add
+from add_0 import add
 
-    a = 1
-    b = 2
-    result = add(a, b)
-    print(f"{a} + {b} = {result}")
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
