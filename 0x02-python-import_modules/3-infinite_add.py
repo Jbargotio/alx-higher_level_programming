786d4b57461986c9dbd83f534261863931e023b2
Jbargotio <jbargotio@gmail.com>
2022-06-02 04:52:59 -0700

    Print result of addition of arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,7 +2,11 @@
 
 if __name__ == "__main__":
     import sys
+
     a = 0
-    for b in range(1,len(sys.argv)):
-                a += int(sys.argv[b])
-    print(f"{a}")        
+    if len(sys.argv) == 1:
+        print("0")
+    else:
+        for b in range(1, len(sys.argv)):
+            a += int(sys.argv[b])
+        print(f"{a}")
