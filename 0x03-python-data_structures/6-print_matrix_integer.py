2b346db7669a1acaa171459f89dac7e8cf9834ac
Jbargotio <jbargotio@gmail.com>
2022-06-06 03:54:24 -0700

    Print elements in an array

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+# 6-print_matrix_integer.py
+
+
 def print_matrix_integer(matrix=[[]]):
-    x = '\n'.join([''.join(['{:3}'.format(item) for item in row]) for row in matrix])
-    print(x)
+    """Print a matrix of integers."""
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
