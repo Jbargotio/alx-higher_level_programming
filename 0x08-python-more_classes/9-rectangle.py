4578b97161776a842bcda1e64080aa344dda139b
Jbargotio <jbargotio@gmail.com>
2022-06-27 05:12:21 -0700

    Tasks 9 and 10

---
+++ 0x08-python-more_classes/9-rectangle.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""Creation of class Rectangle"""
+
+
+class Rectangle:
+    """Definition the class"""
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        """Initialization of Rectangle"""
+
+        self.__width = width
+        self.__height = height
+        type(self).number_of_instances += 1
+
+    @property
+    def width(self):
+        """Setting of width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Requirements for width"""
+        if not type(int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """Setting height of rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Requirements for height"""
+        if not type(int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """Calculating area"""
+        return self.height * self.width
+
+    def perimeter(self):
+        """Calculating perimeter"""
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.height + self.width)
+
+    def __str__(self):
+        """Print the rectangle using #"""
+
+        if self.height == 0 or self.width == 0:
+            return ""
+
+        rect = []
+        for i in range(self.__height):
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append('\n')
+        return("".join(rect))
+
+    def __repr__(self):
+        """String representation of Rectangle"""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
+
+    def __del__(self):
+        """Print this message for every Rectangle deletion."""
+        print('Bye rectangle...')
+        type(self).number_of_instances -= 1
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """ Returns biggest rectangle"""
+        if isinstance(rect_1, Rectangle) is False:
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if isinstance(rect_2, Rectangle) is False:
+            raise TypeError('rect_2 must be an instance of Rectangle')
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        if rect_2.area() > rect_1.area():
+            return rect_2
+
+    @classmethod
+    def square(cls, size=0):
+        """Return a new Rectangle with width and height equal to size.
+        Args:
+            size (int): The width and height of the new Rectangle.
+        """
+        return (cls(size, size))
